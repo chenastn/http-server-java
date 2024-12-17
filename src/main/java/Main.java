d5117ff386f49d29bfb1dbbeeebefd46421fe0ff
Austin Chen <chenastn@gmail.com>
2024-12-17 03:17:07 -0500

    response body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,32 +11,40 @@ public class Main {
     public static void main(String[] args) {
         final List<String> VALID_PATHS = new ArrayList<>();
         VALID_PATHS.add("/");
-
+        VALID_PATHS.add("/echo/abc");
 
         try {
             ServerSocket serverSocket = new ServerSocket(4221);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
 
             while(true) {
-                Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+                Socket clientSocket = serverSocket.accept();
                 System.out.println("accepted new connection");
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String request = in.readLine();
+                System.out.println("request: " + request); // debugging
 
                 String path = "";
+                String responseString = "";
                 if (request != null) {
-                    String[] query = request.split(" ");
-                    if (query.length > 1) {
-                        path = query[1];
+                    String[] requestParts = request.split(" ");
+                    String[] intermediateResponseParts = request.split(" ");
+                    String[] responseParts = intermediateResponseParts[1].split("/");
+
+                    if (requestParts.length > 1) {
+                        path = requestParts[1];
+                    }
+                    if (responseParts.length > 1) {
+                        responseString = responseParts[2];
                     }
                 }
+                System.out.println("path: " + path); // debugging
+                System.out.println("string portion: " + responseString); // debugging
 
                 OutputStream out = clientSocket.getOutputStream();
                 if (VALID_PATHS.contains(path)) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n" + responseString).getBytes());
                 } else {
                     out.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
                 }
