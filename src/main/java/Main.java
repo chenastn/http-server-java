5efb2c8810e39cfc78823bc1ab81aa62c3209703
Austin Chen <chenastn@gmail.com>
2024-12-21 20:44:56 -0500

    file output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -29,38 +31,40 @@ public class Main {
         System.out.println("request: " + request); // debugging
 
         if (request == null) {
-            sendResponse(clientSocket, null);
+            sendResponse(clientSocket, null, null);
             return;
         }
 
         String[] requestParts = request.split(" ");
         if (requestParts.length < 2) {
-            sendResponse(clientSocket, null);
+            sendResponse(clientSocket, null, null);
             return;
         }
 
         String path = requestParts[1];
         System.out.println("path: " + path); // debugging
         if (path.startsWith("/echo")) {
-            String echoContent = path.substring(6); // omits the first six characters
-            sendResponse(clientSocket, echoContent);
+            sendResponse(clientSocket, "text/plain", path.substring(6));
         } else if (path.startsWith("/user-agent")) {
-            sendResponse(clientSocket, getUserAgent(in));
+            sendResponse(clientSocket, "text/plain", getUserAgent(in));
+        } else if (path.startsWith("/files")) {
+            sendResponse(clientSocket, "application/octet-stream", getFileContent(path));
         } else if (path.equals("/")) {
-            sendResponse(clientSocket, path);
+            sendResponse(clientSocket, "text/plain", path);
         } else {
-            sendResponse(clientSocket, null);
+            sendResponse(clientSocket, "text/plain", null);
         }
     }
 
-    private static void sendResponse(Socket clientSocket, String body) throws IOException {
+    private static void sendResponse(Socket clientSocket, String contentType, String body) throws IOException {
         OutputStream out = clientSocket.getOutputStream();
         if (body != null && body.equals("/")) {
             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
         } else if (body != null && !body.isEmpty()) {
             out.write(("HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/plain\r\n"
-                    + "Content-Length: " + body.length()
+                    + "Content-Type: " + contentType
+                    + "\r\n"
+                    + "Content-Length: " + body.getBytes().length
                     + "\r\n\r\n"
                     + body).getBytes());
         } else {
@@ -82,5 +86,25 @@ public class Main {
                 }
             }
         }
+    }
+
+    private static String getFileContent(String path) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(getFilePath(path)))) {
+            System.out.println("getFilePath(path): " + getFilePath(path)); // debugging
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+            System.out.println("content = " + content.toString().trim()); // debugging
+            return content.toString().trim();
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
+    private static String getFilePath(String requestPath) {
+        String fileName = requestPath.substring(7);
+        return "/tmp/" + fileName;
     }
 }
